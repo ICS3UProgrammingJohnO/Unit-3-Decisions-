af569ae9e68c28088995ba2a5dee2dac62d90495
John Omage <47784108+s422862@users.noreply.github.com>
2020-04-23 19:08:07 -0400

    Add files via upload

---
+++ WalkingManJohnO/WalkingManJohnO/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WalkingManJohnO
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picMan = new System.Windows.Forms.PictureBox();
+            this.btnWalk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picMan
+            // 
+            this.picMan.Image = global::WalkingManJohnO.Properties.Resources.walk10;
+            this.picMan.InitialImage = global::WalkingManJohnO.Properties.Resources.walk10;
+            this.picMan.Location = new System.Drawing.Point(202, 142);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(245, 243);
+            this.picMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMan.TabIndex = 2;
+            this.picMan.TabStop = false;
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWalk.Location = new System.Drawing.Point(231, 53);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(191, 45);
+            this.btnWalk.TabIndex = 3;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnWalk);
+            this.Controls.Add(this.picMan);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking man by John O";
+            this.Load += new System.EventHandler(this.frmWalkingMan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picMan;
+        private System.Windows.Forms.Button btnWalk;
+    }
+}
+
