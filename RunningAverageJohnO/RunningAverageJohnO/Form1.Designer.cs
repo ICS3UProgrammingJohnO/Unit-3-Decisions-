9368e8b2591b785699631891c447f78433b68937
John Omage <47784108+s422862@users.noreply.github.com>
2020-04-25 19:30:50 -0400

    Add files via upload

---
+++ RunningAverageJohnO/RunningAverageJohnO/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RunningAverageJohnO
+{
+    partial class frmRunningAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverageAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(179, 128);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(147, 13);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "Enter a number from 0 to 100:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(368, 121);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Location = new System.Drawing.Point(179, 173);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(81, 13);
+            this.lblExit.TabIndex = 4;
+            this.lblExit.Text = "(Enter -1 to exit)";
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(182, 229);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(144, 44);
+            this.btnAverage.TabIndex = 5;
+            this.btnAverage.Text = "Calculate Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblAverageAnswer
+            // 
+            this.lblAverageAnswer.AutoSize = true;
+            this.lblAverageAnswer.Location = new System.Drawing.Point(191, 347);
+            this.lblAverageAnswer.Name = "lblAverageAnswer";
+            this.lblAverageAnswer.Size = new System.Drawing.Size(35, 13);
+            this.lblAverageAnswer.TabIndex = 6;
+            this.lblAverageAnswer.Text = "label1";
+            // 
+            // frmRunningAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAverageAnswer);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "frmRunningAverage";
+            this.Text = "Running Average by John O";
+            this.Load += new System.EventHandler(this.frmRunningAverage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblExit;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverageAnswer;
+    }
+}
+
---
+++ RunningAverageJohnO/RunningAverageJohnO/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningAverageJohnO
+{
+    public partial class frmRunningAverage : Form
+    {
+        // set the variables
+        double sum = 0;
+        double numbersByNumbers = 0;
+
+        public frmRunningAverage()
+        {
+            InitializeComponent();
+            // hide the labels
+            lblAverageAnswer.Hide();
+        }
+
+        private void frmRunningAverage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            // Show the labels
+            lblAverageAnswer.Show();
+
+            // declare local variables
+            double userValue;
+            double average;
+
+            // get the number from the textbox 
+            userValue = double.Parse(txtNumber.Text);
+
+            // increment the number
+            numbersByNumbers++;
+
+            // calculate the average
+            sum = sum + userValue;
+            average = sum / numbersByNumbers;
+
+            // display the average of the running
+            lblAverageAnswer.Text = Convert.ToString(average);
+
+            // if the user enters -1, display a Goodbye message and disable buttons
+            if (userValue == -1)
+            {
+                MessageBox.Show("Running average ended.", "Running Average");
+                this.btnAverage.Enabled = false;
+                this.txtNumber.Visible = false;
+                this.Close();
+            }
+        }
+    }
+}
