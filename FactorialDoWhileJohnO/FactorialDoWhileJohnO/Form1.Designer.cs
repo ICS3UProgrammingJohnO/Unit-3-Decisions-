4a87484d06ef771b2b48c01d5e8ce8214c70f98c
John Omage <47784108+s422862@users.noreply.github.com>
2020-04-25 18:02:24 -0400

    Add files via upload

---
+++ FactorialDoWhileJohnO/FactorialDoWhileJohnO/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace FactorialDoWhileJohnO
+{
+    partial class frmFactorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstFactorial = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(109, 75);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(223, 33);
+            this.lblNumber.TabIndex = 5;
+            this.lblNumber.Text = "Enter a number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(385, 75);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(166, 38);
+            this.txtNumber.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(265, 138);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(134, 40);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstFactorial
+            // 
+            this.lstFactorial.FormattingEnabled = true;
+            this.lstFactorial.Location = new System.Drawing.Point(213, 207);
+            this.lstFactorial.Name = "lstFactorial";
+            this.lstFactorial.Size = new System.Drawing.Size(250, 160);
+            this.lstFactorial.TabIndex = 8;
+            this.lstFactorial.SelectedIndexChanged += new System.EventHandler(this.lstFactorial_SelectedIndexChanged);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(221, 386);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(205, 31);
+            this.lblAnswer.TabIndex = 9;
+            this.lblAnswer.Text = "factorial answer";
+            // 
+            // frmFactorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstFactorial);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "frmFactorial";
+            this.Text = "Factorial by John O";
+            this.Load += new System.EventHandler(this.frmFactorial_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstFactorial;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ FactorialDoWhileJohnO/FactorialDoWhileJohnO/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialDoWhileJohnO
+{
+    public partial class frmFactorial : Form
+    {
+        public frmFactorial()
+        {
+            InitializeComponent();
+            lblAnswer.Hide();
+        }
+
+        private void lstFactorial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmFactorial_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            Double factorialAnswer;
+            Double factorialNumber;
+            int factorialCounter;
+
+            // clear the items from the listbox
+            this.lstFactorial.Items.Clear();
+
+            // initialize the final answer to 1
+            factorialAnswer = 1;
+
+            // get the number from the user
+            factorialNumber = Convert.ToDouble(this.txtNumber.Text);
+
+            // set the counter to 0
+            factorialCounter = 0;
+
+            // multiply the counter by the next incremented number until it reaches the user's number
+            do
+            {
+                // increment the counter by 1
+                factorialCounter = factorialCounter + 1;
+
+                // list the counter number in the listbox for the user to see
+                lstFactorial.Items.Add(factorialCounter);
+
+                // multiply the counter by the answer
+                factorialAnswer = factorialAnswer * factorialCounter;
+
+            } while (factorialCounter < factorialNumber);
+
+            // convert the factorialAnswer to a String and insert it into the label
+            this.lblAnswer.Text = this.txtNumber.Text + "! = " + Convert.ToString(factorialAnswer);
+
+            // show the label answer
+            lblAnswer.Show();
+        }
+    }
+}
